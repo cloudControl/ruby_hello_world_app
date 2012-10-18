936e20926d93305d130a1d831b3fe6533e89e81f
Dimitris Verraros <vervas.g@gmail.com>
2012-10-18 16:36:51 +0200

    reformation

--- helloworld.rb
+++ helloworld.rb
@@ -2,5 +2,11 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+     "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">"+
+     "<HTML>"+
+     "<HEAD><TITLE>Ruby on cloudControl</TITLE></HEAD>"+
+     "<BODY>"+
+     "<center><h1>Hello World!</h1></center>"+
+     "</BODY>"+
+     "</HTML>"
 end
